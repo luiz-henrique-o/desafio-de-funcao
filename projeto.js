40f20ca88192f5a891c71cfc690c9dcd36954952
luiz-henrique-o <luihenriquedeoliveirasilva61@gmail.com>
2024-01-04 16:33:39 -0300

    meu secundo commit

---
+++ projeto.js
@@ -0,0 +1,25 @@
+
+
+function experiencia() {
+    let saldoDeVitorias = 109
+    if (saldoDeVitorias <= 10){
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de ferro") 
+    } else if (saldoDeVitorias >= 11 || saldoDeVitorias <= 20 ){
+         console.log("O heroi tem o saldo de vitorias 109 e está no nivel de bronze") 
+    } else if (saldoDeVitorias >= 21 || saldoDeVitorias <= 50 ){
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de prata") 
+    } else if (saldoDeVitorias >= 51 || saldoDeVitorias <= 80 ){
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de ouro") 
+    } else if (saldoDeVitorias >= 81 || saldoDeVitorias <= 90){
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de diamante") 
+    } else if (saldoDeVitorias >= 91 || saldoDeVitorias <= 100){
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de lendario") 
+    } else (saldoDeVitorias >= 101)
+        console.log("O heroi tem o saldo de vitorias 109 e está no nivel de imortal") 
+    }
+    
+    
+    
+    
+    experiencia()
+
